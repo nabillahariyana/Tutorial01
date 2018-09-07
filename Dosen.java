b36233e692cac6c35035c34a1d7bcb65e45bfe4c
Nabilla Hariyana <nabilla.hari@gmail.com>
2018-09-07 14:41:16 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,18 @@
+
+public class Dosen {
+	private String namaDosen;
+
+	public Dosen(String namaDosen) {
+		super();
+		this.namaDosen = namaDosen;
+	}
+
+	public String getNamaDosen() {
+		return namaDosen;
+	}
+
+	public void setNamaDosen(String namaDosen) {
+		this.namaDosen = namaDosen;
+	}
+	
+}
